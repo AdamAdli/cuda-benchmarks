4a39f76840ef8b44b1e969057b68f8ebbd4cb8f5
Jim Gao <jim@jimgao.tk>
2021-10-02 19:08:48 -0400

    Test

---
+++ sputnik/test.cu.cc
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include "sputnik/sputnik.h"
+#include "sputnik/cuda_utils.h"
+#include "sputnik/matrix_utils.h"
+#include "sputnik/spmm/cuda_spmm.h"
+#include "sputnik/spmm/spmm_config.h"
+#include "sputnik/test_utils.h"
+
+
+int main() {
+	std::cout << "Hello World" << std::endl;
+}
+
